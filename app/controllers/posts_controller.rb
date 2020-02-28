db23ee03f58c3cf044a6b70624696212b2fbc5f6
JasonP77 <llvslr77@gmail.com>
2020-02-28 16:24:45 -0500

    first part

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,21 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @user = User.find(params[:id])
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to post_path(@user)
+  end
+
+  def post_params
+    params.require(:post).permit(:category_id, :user_id, :content, :title)
   end
 end
